8ae204fed9b6228706b4fb500475a935d65f09e4
Kwasserman43 <132620487+Kwasserman43@users.noreply.github.com>
2024-05-22 09:36:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     //Buttons that lead from main menu to each modes and vice versa
     const cr_button = document.getElementById("cr_button");
-    const ct_button = document.getElementById("ct_button");
+    const ct_button2 = document.getElementById("ct_button2");
 
     cr_button.addEventListener('click', function() {
         navigateToNewPage("chordrecognition", "chordrecognition")
     })
 
-    ct_button.addEventListener('click', function() {
-        navigateToNewPage('chordtransition', "chordtransition")
+    ct_button2.addEventListener('click', function() {
+        window.location.href = "website/home.html"
     })
    
 });
